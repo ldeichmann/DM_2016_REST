c3768ca1dcd06f02f2a86751afb8b844ec53c2e7
Lukas Deichmann <github@pezam.de>
2016-04-19 10:42:19 +0200

    add initial files

---
+++ lib/types/kuenstler.dart
@@ -0,0 +1,15 @@
+class kuenstler {
+
+  var id;
+  var name;
+  var biographie;
+  var herkunft;
+
+  kuenstler(id, name, biographie, herkunft) {
+    this.id = id;
+    this.name = name;
+    this.biographie = biographie;
+    this.herkunft = herkunft;
+  }
+
+}
---
+++ lib/types/titel.dart
@@ -0,0 +1,19 @@
+class titel {
+
+  var id;
+  var name;
+  var preis;
+  var pfad;
+  var laenge;
+  var kuenstler_id;
+
+  titel(id, name, preis, pfad, laenge, kuenstler_id) {
+    this.id = id;
+    this.name = name;
+    this.preis = preis;
+    this.pfad = pfad;
+    this.laenge = laenge;
+    this.kuenstler_id = kuenstler_id;
+  }
+
+}
