20c77db2867c9ecd2605b58b8896a93dc5939cee
Lukas Deichmann <github@pezam.de>
2016-04-19 11:03:09 +0200

    further changes

--- lib/rest.dart
+++ lib/rest.dart
@@ -1,6 +1,13 @@
 import 'package:start/start.dart';
+import 'package:dm_rest/types/titel.dart';
+import 'package:dm_rest/types/kuenstler.dart';
 
 void main() {
+
+  List<kuenstler> kuenstlerList = new List();
+  List<titel> titelList = new List();
+
+
   start(port: 3000).then((Server app) {
 
     app.static('web');
@@ -16,11 +23,23 @@ void main() {
       socket.on('pong').listen((data) => socket.close(1000, 'requested'));
     });
 
+    app.get('/kuenstler').listen((request) {
+      request.response
+          .header('Content-Type', 'text/html; charset=UTF-8')
+          .send(kuenstlerList.toString());
+    });
+
     app.get('/kuenstler/:id').listen((request) {
 
     });
 
-    app.put('/kuenstler')
+    app.post('/kuenstler').listen((request) {
+      var name = request.param('name');
+      var biographie = request.param('biographie');
+      var herkunft = request.param('herkunft');
+      var new_kuenstler = new kuenstler(kuenstlerList.length, name, biographie, herkunft);
+      kuenstlerList.add(new_kuenstler);
+    });
 
   });
 }
