4e7e5c341a54ff9e059318cdbb84833d0021d078
Jan Lukas Deichmann <github@pezam.de>
2016-04-25 20:22:09 +0200

    implement more interfaces

--- lib/types/album.dart
+++ lib/types/album.dart
@@ -1,10 +1,38 @@
+import 'package:dm_rest/types/kuenstler.dart';
+import 'package:dm_rest/types/titel.dart';
 
+class album {
 
-
-class Album {
-
+  String id;
   String name;
-  kuenstler kuenstler;
+  String k;
+  List<titel> titelList;
+  double preis;
 
+  album(id, name, k, preis) {
+    this.id = id;
+    this.name = name;
+    this.k = k;
+    this.preis = preis;
+    titelList = new List();
+  }
+
+  void addTitel(titel t) {
+    this.titelList.add(t);
+  }
+
+  void removeTitel(titel t) {
+    this.titelList.remove(t);
+  }
+
+  Map toJson() {
+    Map map = new Map();
+    map["id"] = this.id;
+    map["name"] = this.name;
+    map["kuenstler"] = this.k;
+    map["titel"] = this.titelList;
+    map["preis"] = this.preis;
+    return map;
+  }
 
 }
